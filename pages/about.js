6b3db64078b44fcc291eea3aa66069da404640ce
David Mwendwa <klatzdavie19@gmail.com>
2021-12-08 22:32:40 +0300

    feat: pages and routing

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
+const About = () => {
+  return ( 
     <div>
-      <h1>ninja homepage</h1>
+      <h1>About</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
     </div>
-  )
+  );
 }
+ 
+export default About;
