b721b404354130be89bbac88295f7a76364fffda
David Mwendwa <klatzdavie19@gmail.com>
2021-12-08 23:06:49 +0300

    feat: add nav and footer components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>ninja homepage</h1>
+      <Navbar />
+      <h1>Homepage</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+      <Footer />
     </div>
   )
 }
