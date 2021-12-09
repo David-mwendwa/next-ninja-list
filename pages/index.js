2d0a3886903a7befb6a29de53b48529dd125ea76
David Mwendwa <klatzdavie19@gmail.com>
2021-12-09 08:11:26 +0300

    feat: create a layout component

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,12 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
