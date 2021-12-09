ac6e418a4fef5ed4fb1f4c5c5873af147b3b921a
David Mwendwa <klatzdavie19@gmail.com>
2021-12-09 08:45:09 +0300

    feat: add styles

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,27 @@ import Head from 'next/head'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
-import Link from "next/link"
+import Link from "next/Link"
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-      <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book
+      </p>
+      <p className={styles.text}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book
+      </p>
+      <Link href='/ninjas'>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
-  )
+  );
 }
