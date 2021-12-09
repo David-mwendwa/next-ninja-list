9fde5bd6a4aca9bcb9512cda7159613600ed6a17
David Mwendwa <klatzdavie19@gmail.com>
2021-12-09 07:51:14 +0300

    feat: link in between pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
